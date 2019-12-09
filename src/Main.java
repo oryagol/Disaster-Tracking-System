2305ab5bc41d27b83a3552ce03a529aaa47f8270
oryagol <oryagol@gmail.com>
2019-12-09 12:11:43 +0200

    Adding javafx features

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
 
-public class Main {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
 	
 	public static void main(String args[]) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("View/MainView.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.setWidth(900);
+		stage.setHeight(640);
+		stage.setResizable(false);
+		stage.initStyle(StageStyle.UNDECORATED);
+		stage.show();
 		
 	}
 }
